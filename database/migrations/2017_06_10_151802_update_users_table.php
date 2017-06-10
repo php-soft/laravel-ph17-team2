1a58592967de54751b37490b2b2f3d78ffa1987d
kokuu811 <nmtoan188@gmail.com>
2017-06-10 22:34:33 +0700

    Anh huy check tam 3 cai migration dum em

---
+++ database/migrations/2017_06_10_151802_update_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('level');
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('level');
+            $table->dropColumn('created_at');
+            $table->dropColumn('updated_at');
+        });
+    }
+}
