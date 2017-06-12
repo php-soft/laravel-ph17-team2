a6d70bd9156e0b992521a068a9b0e9e1966c020f
kokuu811 <nmtoan188@gmail.com>
2017-06-12 20:21:13 +0700

    create category_attribute_values migration

---
+++ database/migrations/2017_06_12_131918_create_category_attribute_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_attribute_values', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->unsignedInteger('attribute_value_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('attribute_value_id')->references('id')->on('attribute_values');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_attribute_values');
+    }
+}
