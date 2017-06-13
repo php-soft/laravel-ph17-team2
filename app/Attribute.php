3c43c0b97dd081e67ecb9955877ff791d1795bde
kokuu811 <nmtoan188@gmail.com>
2017-06-13 17:32:26 +0700

    fix Attribute model

--- app/Attribute.php
+++ app/Attribute.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Attribute extends Model
 {
     public $timestamps = false;
+
     public $fillable = [
         'name',
         'description'
     ];
+    
     public function attributeValues()
     {
         return $this->hasMany('App\AttributeValues');
