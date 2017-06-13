185100896a71d89a4efef8fd9293602c82d15d0d
kokuu811 <nmtoan188@gmail.com>
2017-06-13 17:13:02 +0700

    create Attribute model

---
+++ app/Attribute.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attribute extends Model
+{
+    public $timestamps = false;
+    public $fillable = [
+        'name',
+        'description'
+    ];
+    public function attributeValues()
+    {
+        return $this->hasMany('App\AttributeValues');
+    }
+}
