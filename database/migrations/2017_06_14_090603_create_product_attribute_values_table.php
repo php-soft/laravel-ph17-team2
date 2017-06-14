f1090cb501129d26bf794bd747e194a99dc12ad5
kokuu811 <nmtoan188@gmail.com>
2017-06-14 16:08:01 +0700

    reup category_attribute_values and product_attribute_values migrations

---
+++ database/migrations/2017_06_14_090603_create_product_attribute_values_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductAttributeValuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attribute_values', function (Blueprint $table) 
+        {
+            $table->increments('id');
+            $table->unsignedInteger('product_id')->nullable();
+            $table->unsignedInteger('attribute_value_id')->nullable();
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->foreign('attribute_value_id')->references('id')->on('attribute_values');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attribute_values');
+    }
+}
