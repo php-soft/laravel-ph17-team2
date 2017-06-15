3ef00085c819d53148380d59bfb02abf65207f74
thuan59dt <thuan59.dt@gmail.com>
2017-06-15 20:11:56 +0700

    create_NewsCategory-model

---
+++ app/NewsCategory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NewsCategory extends Model
+{   
+	public $timestamp = false;
+
+    protected $fillable = [
+        'name',
+        'alias'        
+    ];
+
+    public function news()
+    {
+        return $this->hasMany('App\New');
+    }
+}
