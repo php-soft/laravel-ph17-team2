e3ffe887dd40d96a52480629ea597c92ac1b0b95
thuan59dt <thuan59.dt@gmail.com>
2017-06-15 16:23:27 +0700

    create_reviews-migration

---
+++ database/migrations/2017_06_15_091822_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->foreign('user_id')->references('id')->on('users'); 
+            $table->integer('shop_product_id')->unsigned()->nullable();
+            $table->foreign('shop_product_id')->references('id')->on('shop_products');
+            $table->integer('status');
+            $table->text('content'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
