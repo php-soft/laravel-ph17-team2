e4ec3c67ca751771d529c2e1e7c0f45a7f24a88f
doinguoi1232 <phuquydn@gmail.com>
2017-06-16 18:10:47 +0700

    update order migration

---
+++ database/migrations/2017_06_16_110435_update_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table)
+        {
+            $table->integer('user_id')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function ($table)
+        {
+            $table->dropColumn('user_id');
+        });
+    }
+}
