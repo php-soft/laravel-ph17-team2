4996264f0c702135189df4b52b70685e4c7b0e97
doinguoi1232 <phuquydn@gmail.com>
2017-06-16 15:19:27 +0700

    fix user model

--- app/User.php
+++ app/User.php
@@ -26,4 +26,44 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function news()
+    {
+        return $this->hasMany('App\New');
+    }
+    public function usersRole()
+    {
+        return $this->hasMany('App\UsersRole');
+    }
+    public function profile()
+    {
+        return $this->hasMany('App\Profile');
+    }
+    public function wishlistItem()
+    {
+        return $this->hasMany('App\WishlistItem');
+    }public function review()
+{
+    return $this->hasMany('App\Review');
+}
+    public function abuse()
+    {
+        return $this->hasMany('App\Abuse');
+    }
+    public function order()
+    {
+        return $this->hasMany('App\Order');
+    }
+    public function shopProduct()
+    {
+        return $this->hasMany('App\ShopProduct');
+    }
+    public function returnItem()
+    {
+        return $this->hasMany('App\ReturnItem');
+    }
+    public function shop()
+    {
+        return $this->hasMany('App\Shop');
+    }
 }
