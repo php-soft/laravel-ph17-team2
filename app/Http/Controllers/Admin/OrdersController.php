8b3811089929e834a7336fac4a55296b171b8776
doinguoi1232 <phuquydn@gmail.com>
2017-06-19 20:03:56 +0700

    fix orderdetail

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use DB;
 
 class OrdersController extends Controller
 {
@@ -12,4 +13,26 @@ class OrdersController extends Controller
         $orders = \App\Order::all();
         return view('admin.orders.index')->with('orders', $orders);
     }
+
+    public function show($id)
+    {
+        $orders = \App\Order::find($id);
+        $status = $orders->status;
+        $total_amount = 0;
+        foreach ($orders->orderProducts as $product) {
+            $amount = $product->price*$product->quantity;
+            $total_amount = $amount + $total_amount;
+        }
+        $users = DB::table('orders')->join('users', 'users.id', '=','orders.user_id')->join('profiles', 'users.id', '=','profiles.user_id')->where('users.id', '=', $id)->get();
+        return view('admin.orders.show')->with('orders', $orders)->with('users',$users)->with('status', $status)->with('total_amount',$total_amount);
+    }
+
+    public function edit(Request $request, $id)
+    {
+        $orders = \App\Order::findOrFail($id);
+        $orders->update($request->all());
+        return redirect()->route('adminOrders', ['id' => $id]);
+    }
+
 }
+
