05e93a84cfb1fdeb842ba4ae900546abbc5b3313
kokuu811 <nmtoan188@gmail.com>
2017-06-19 19:48:19 +0700

    fix migration-model-seeder chapter 2

--- database/seeds/CategoryAttributeValuesTableSeeder.php
+++ database/seeds/CategoryAttributeValuesTableSeeder.php
@@ -14,9 +14,9 @@ class CategoryAttributeValuesTableSeeder extends Seeder
         DB::table('category_attribute_values')->insert([
             ['id' => 1, 'category_id' => 1, 'name' => 'Màu'],
             ['id' => 2, 'category_id' => 22, 'name' => 'Size Áo'],
-            ['id' => 3, 'category_id' => 22, 'name' => 'Màu'],
-            ['id' => 4, 'category_id' => 38, 'name' => 'Màu'],
-            ['id' => 5, 'category_id' => 38, 'name' => 'Size Quần']
+            ['id' => 3, 'category_id' => 22, 'name' => 'Màu'],    
+            ['id' => 4, 'category_id' => 38, 'name' => 'Size Quần'],
+            ['id' => 5, 'category_id' => 38, 'name' => 'Màu']
         ]);
     }
 }
