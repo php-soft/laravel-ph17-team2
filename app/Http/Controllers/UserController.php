416898fcc8de3f6f33c08bbeb9d4979537495803
khoivinhphan <khoivinhphan@gmail.com>
2017-06-19 17:05:16 +0700

    Demo manager user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+class UserController extends Controller
+{
+    public function index(){
+    	$users = User::all();
+    	return view('user.index')->with('users',$users);
+    }
+}
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h2>Manager User</h2>
+		<h3><a href="" title=""><span class="glyphicon glyphicon-trash"></span>Thùng Rác</a></h3>
+
+		@foreach($users as $user)
+		<table class="table" >
+			<tr>
+				<th width="200px">{{$user->name}}</th>
+				<th width="500px">{{$user->email}}</th>
+				<th>
+					<a href="" title=""><span class="glyphicon glyphicon-trash"></span>Xóa User</a>
+					<a href="" title=""><span class="glyphicon glyphicon-edit"></span>Chỉnh sửa User</a>
+				</th>
+			</tr>
+		</table>
+		@endforeach
+
+	</div>
+
+</body>
+</html>
