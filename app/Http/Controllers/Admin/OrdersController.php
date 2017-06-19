70316d7badcd05156d0aa4cd6b3e57f460621c48
doinguoi1232 <phuquydn@gmail.com>
2017-06-19 20:24:14 +0700

    fix orderdetail

--- app/Http/Controllers/Admin/OrdersController.php
+++ app/Http/Controllers/Admin/OrdersController.php
@@ -23,8 +23,10 @@ class OrdersController extends Controller
             $amount = $product->price*$product->quantity;
             $total_amount = $amount + $total_amount;
         }
-        $users = DB::table('orders')->join('users', 'users.id', '=','orders.user_id')->join('profiles', 'users.id', '=','profiles.user_id')->where('users.id', '=', $id)->get();
-        return view('admin.orders.show')->with('orders', $orders)->with('users',$users)->with('status', $status)->with('total_amount',$total_amount);
+        $users = DB::table('orders')->join('users', 'users.id', '=','orders.user_id')->join('profiles', 'users.id', '=','profiles.user_id')
+        ->where('users.id', '=', $id)->get();
+        return view('admin.orders.show')->with('orders', $orders)->with('users',$users)->with('status', $status)
+        ->with('total_amount',$total_amount);
     }
 
     public function edit(Request $request, $id)
@@ -33,6 +35,5 @@ class OrdersController extends Controller
         $orders->update($request->all());
         return redirect()->route('adminOrders', ['id' => $id]);
     }
-
 }
 
