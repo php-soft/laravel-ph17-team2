15aa66e32f9f5cee5686d879da4e3a305591c920
thuan59dt <thuan59.dt@gmail.com>
2017-06-20 10:00:49 +0700

    home

---
+++ app/Http/Controllers/TypeController.blade.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+use App\Type;
+use Illuminate\Http\Request;
+class TypeController extends Controller
+{
+    public function show($id)
+    {
+        $type = Type::findOrFail($id);
+        return view('types.show')
+            ->with('type', $type);
+    }
+}
