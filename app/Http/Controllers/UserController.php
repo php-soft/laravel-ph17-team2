42ac67b938c197910e0bb127352dfb68194ddcbf
khoivinhphan <khoivinhphan@gmail.com>
2017-06-20 15:59:03 +0700

    manager-user #190

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,21 @@ class UserController extends Controller
 {
     public function index()
     {
-    	$users = User::all();
-    	return view('user.index')->with('users',$users);
+        $users = User::all();
+        return view('user.index')->with('users',$users);
     }
 
     public function destroy(User $user)
     {
-    	$user->delete();
-    	return redirect('user');
+        $user->delete();
+        return redirect('user');
     }
 
     public function edit(User $user)
     {
-    	return view('user.edit')->with('user',$user);
+        return view('user.edit')->with('user',$user);
     }
-    
+
     public function update(User $user)
     {
         $user->update(Input::all());
