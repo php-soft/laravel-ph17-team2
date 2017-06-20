a7d8b95ea40a456dc6d76feb2de58c813b92406b
kokuu811 <nmtoan188@gmail.com>
2017-06-20 17:44:01 +0700

    show products chap 7

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () 
-{
+Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
     Route::get('/orders', 'OrdersController@index')->name('ordersAdmin');
     Route::get('/orders/{id}', 'OrdersController@show')->name('adminOrders');
     Route::put('/orders/{id}/edit', 'OrdersController@edit')->name('adminOrderEdit');
