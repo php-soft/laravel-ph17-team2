20d786a8f6ebba464036451c4a92c6e0d74d8762
kokuu811 <nmtoan188@gmail.com>
2017-06-20 17:20:58 +0700

    show products chap 3

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -8,24 +8,24 @@
 				    <div>
 					    <a href="{{ url('/products/' . $product->id) }}"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-90/test.png" alt="This is an image" alt="{!! $product->image !!}"></a>
 					</div>
-					<div>
+				    <div>
 					    <span class="glyphicon glyphicon-heart-empty" style="font-size:36px;color:red"></span>
-					</div>
-					<div style="color:red">
-						{{ $product->price }}
-					</div>
-					<div>
-						{{ $product->description }}
-					</div>
-					<div>
+				    </div>
+				    <div style="color:red">
+					    {{ $product->price }}
+				    </div>
+				    <div>
+					    {{ $product->description }}
+				    </div>
+				    <div>
 					    <span class="glyphicon glyphicon-tag" style="font-size:24px"></span>{{ $product->buys }}
 					    <span class="glyphicon glyphicon-eye-open" style="font-size:24px"></span>{{ $product->view }}
-					</div>
-					{{-- <div>
+				    </div>
+				    {{-- <div>
 					    <p>{{ $product->shopProducts->shop->name }}</p>
-					</div> --}}
+				    </div> --}}
 				</div>
-			@endforeach
-		</div>
+		    @endforeach
+	    </div>
     </div>
 @stop
