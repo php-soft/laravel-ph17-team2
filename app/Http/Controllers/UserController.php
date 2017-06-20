49024c288557a199c192e0320c5ce11b7f627b12
khoivinhphan <khoivinhphan@gmail.com>
2017-06-20 15:33:13 +0700

    create_manager_user #194

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,22 +1,28 @@
 <?php
 namespace App\Http\Controllers;
+
 use Input;
 use Illuminate\Http\Request;
+
 use App\User;
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
     	$users = User::all();
     	return view('user.index')->with('users',$users);
     }
-    public function destroy(User $user){
+    public function destroy(User $user)
+    {
     	$user->delete();
     	return redirect('user');
     }
-    public function edit(User $user){
+    public function edit(User $user)
+    {
     	return view('user.edit')->with('user',$user);
     }
-    public function update(User $user){
+    public function update(User $user)
+    {
         $user->update(Input::all());
         return redirect('user');
     }
