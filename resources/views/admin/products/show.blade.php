beaece49c2860339ff940278b5f063dbcfc07909
kokuu811 <nmtoan188@gmail.com>
2017-06-21 14:14:10 +0700

    show products chap 11

--- resources/views/admin/products/show.blade.php
+++ resources/views/admin/products/show.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.admin')
 
 @section('content')
+<a href="{{ route('products') }}">Back to list products</a>
 <div class="container-fluid">
+    <label for="shop">Sản phẩm bán từ shop: </label>
     @foreach($shops as $shop)
         {!! $shop->name !!}
     @endforeach
     <br>
-    <img src="{{ $product->image }}" alt="{{ $product->image }}" id="image">
+    <span title="Đã có {!! $product->buys !!} lượt mua">
+        <span class="glyphicon glyphicon-tag" style="font-size:24px"></span>{!! $product->buys !!}
+    </span>
+    <span title="Đã có {!! $product->view !!} lượt views">
+        <span class="glyphicon glyphicon-eye-open" style="font-size:24px"></span>{!! $product->view !!}
+    </span>
+    <br>
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-90/test.png" alt="{{ $product->image }}" id="image">
     <br>
     <label for="name">Tên</label>
     <p id="name">{{ $product->name }}</p>
@@ -19,5 +28,19 @@
         {!! $attribute->value !!}
         <br>
     @endforeach
+    <label for="quantity">Số lượng:</label> <input type="number" min="1" max="9999" autofocus="autofocus" required value="1">
+    <div>Sản phẩm liên quan</div>
+    <br>
+    <br>
+    <div style="border-top: 2px solid black">
+        <br>
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#">Chi tiết sản phẩm</a></li>
+            <li class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#">Đánh giá phản hồi</a>
+            </li>
+            <li><a href="#">Hỏi đáp</a></li>
+        </ul>
+    </div>
 </div>
 @stop
