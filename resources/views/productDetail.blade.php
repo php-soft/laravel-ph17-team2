ec90690e3fdbeaf7a95db604629525d8f4664ea9
kokuu811 <nmtoan188@gmail.com>
2017-06-22 21:29:34 +0700

    decorating productDetail

--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -1,41 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <label for="shop">Sản phẩm bán từ shop: </label>
-    {!! $shopProduct->shop->name !!}
-    <br>
-    <span title="Đã có {!! $shopProduct->product->buys !!} lượt mua">
-        <span class="glyphicon glyphicon-tag" style="font-size:24px"></span>{!! $shopProduct->product->buys !!}
-    </span>
-    <span title="Đã có {!! $shopProduct->product->view !!} lượt views">
-        <span class="glyphicon glyphicon-eye-open" style="font-size:24px"></span>{!! $shopProduct->product->view !!}
-    </span>
-    <br>
-    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-90/test.png" alt="{{ $shopProduct->product->image }}" id="image">
-    <br>
-    <label for="name">Tên</label>
-    <p id="name">{{ $shopProduct->product->name }}</p>
-    <br>
-    <label for="price">Giá</label>
-    <p id="price">{{ $shopProduct->product->price }} VNĐ</p>
-    <div>
-    <label for="">Size  màu</label>
-    @if (!empty($attributes))
-        @foreach($attributes as $attribute)
-            {!! $attribute->value !!}
-        @endforeach
-    @endif
-    <p>
-        <label for="quantity">Số lượng:</label> <input type="number" min="1" max="9999" autofocus="autofocus" required value="1">&nbsp;&nbsp;(Còn lại 
-        {!! $shopProduct->quantity !!}
-         sản phẩm)
-     </p>
-</div>
+<div class="container font">
+    <div class="row" style="margin: 5px 4%">
+        <div class="well">
+            <div class="pull-left">
+                <img src="{!! $shopProduct->shop->logo !!}" alt="{!! $shopProduct->shop->logo !!}">
+            </div>
+            <label for="shop" class="lb">Sản phẩm bán từ shop: </label>
+            {!! $shopProduct->shop->name !!}
+        </div>
+        <div class="block-img-attr">
+            <div class="block-pro-img">
+                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-90/test.png" alt="{{ $shopProduct->product->image }}" width="350px" height="350px">
+                <div class="small-img">
+                    <ul>
+                        @foreach($shopProduct->product->productImages as $productImage)
+                            <li><img src="{!! $productImage->thumb !!}" alt="{!! $productImage->thumb !!}"></li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="block-pro-attr">
+                <div class="box-name">
+                    <h1><strong>{!! $shopProduct->product->name !!}</strong></h1>
+                </div>
+                <div class="box-rating">
+                    <span></span>
+                    <span class="glyphicon glyphicon-tag box-rating-span" title="Đã có {!! $shopProduct->product->buys !!} lượt mua"></span>{!! $shopProduct->product->buys !!}
+                    </span>
+                    <span class="glyphicon glyphicon-eye-open" title="Đã có {!! $shopProduct->product->view !!} lượt views"></span>{!! $shopProduct->product->view !!}
+                </span>
+                </div>
+                <div class="box-price">
+                    <p>{{ $shopProduct->product->price }} đ</p>
+                </div>
+                <div>
+                <div class="form-group">
+                    {{Form::open()}}
+                        {{ Form::hidden('id', $shopProduct->product->id) }}
+                        {{ Form::hidden('name', $shopProduct->product->name) }}
+                        {{ Form::hidden('price', $shopProduct->product->price) }}
+                        {{ Form::hidden('image', $shopProduct->product->image) }}
+                        <div class="form-attr">
+                            <div class="form-controls" style="height: 45px;">
+                                <div class="form-lb">
+                                    {!! Form::label('size', 'Size Màu', ['class' => 'form-lb-lb']) !!}
+                                </div>
+                                <div class="attr">
+                                    @foreach($attributes as $attribute)
+                                        {!! $attribute->value !!}
+                                    @endforeach
+                                </div>
+                            </div>
+                            <div>
+                                <div class="form-lb">
+                                    {!! Form::label('qty', 'Số lượng', ['class' => 'form-lb-lb']) !!}
+                                </div>
+                                <div class="form-controls" style="height: 45px;">
+                                    {{ Form::number('qty', 1, ['class'=>'form-control form-quantity', 'min' => 1, 'max' => 99, 'size' => 1]) }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="order">
+                            <button class="btn btn-success w3-btn btn-add">Thêm vào giỏ hàng <span class="glyphicon glyphicon-shopping-cart"></span></button>
+                        </div>
+                    {{ Form::close() }}
+                </div>
+            </div>
+         </div>
+     </div>
     <div>Sản phẩm liên quan</div>
     <br>
     <br>
-    <div style="border-top: 2px solid black">
+    <div class="info">
         <br>
         <ul class="nav nav-tabs">
             <li class="active"><a href="#">Chi tiết sản phẩm</a></li>
