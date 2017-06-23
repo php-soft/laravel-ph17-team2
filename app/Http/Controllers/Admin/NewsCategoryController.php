fc27cdd83304e0f1d11d3b0f9f734f4d7572e594
Thien Hoang <jaynup3ky@gmail.com>
2017-06-23 21:54:33 +0700

    Fix feate manage news category

--- app/Http/Controllers/Admin/NewsCategoryController.php
+++ app/Http/Controllers/Admin/NewsCategoryController.php
@@ -7,48 +7,28 @@ use Illuminate\Support\Facades\Input;
 use App\Http\Controllers\Controller;
 
 class NewsCategoryController extends Controller
-{
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+{   
     public function index()
     {
         $newscategories = \App\NewsCategory::All();
         return view('admin/newscategory/index')->with('newscategories', $newscategories);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function create()
     {
         $newscategory_cr = \App\NewsCategory::pluck('name', 'id');
         return view('admin/newscategory/create')->with('newscategory_cr', $newscategory_cr);
     }
   
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit($id)
     {
         $newscategory = \App\NewsCategory::find($id);
         return view('admin/newscategory/create')->with('newscategory', $newscategory);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update($id)
     {
         $newscategory = \App\NewsCategory::find($id);
@@ -63,12 +43,7 @@ class NewsCategoryController extends Controller
         return redirect('/admin/newscategory')->withSuccess('Cat has been created.');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function delete($id)
     {
         $newcategory = \App\NewsCategory::find($id);
