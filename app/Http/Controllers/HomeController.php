acf61cbcf848b4e26c3c8e0807ab412b98ef2f68
thuan59dt <thuan59.dt@gmail.com>
2017-06-24 22:10:38 +0700

    home_product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,10 +9,14 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $products = DB::table('products')->select('id','name','image','price')->orderBy('id','DESC')->skip(0)->limit(2)->get();
-        
-        $sp = DB::table('products')->select('id','name','image','price','buys')->orderBy('buys','DESC')->limit(2)->get();
-        
+        $products = DB::table('products')
+	        ->select('id','name','image','price')
+	        ->orderBy('id','DESC')
+	        ->limit(2)->get();
+        $sp = DB::table('products')
+	        ->select('id','name','image','price','buys')
+	        ->orderBy('buys','DESC')
+	        ->limit(2)->get();
         return view('home',compact('products','sp'));
     }
 }
