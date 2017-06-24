48944941b910a2d1cac5114ffc1ffee259cd3ca5
doinguoi1232 <phuquydn@gmail.com>
2017-06-25 02:33:33 +0700

    shoppingproductdetail

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -13,7 +13,8 @@ class OrderController extends Controller
 {
     public function show()
     {
-        $order = \App\Order::pluck('name', 'id', 'phone', 'address', 'shipping_name', 'shipping_address', 'shipping_phone', 'voucher_code');
+        $order = \App\Order::pluck('name', 'id', 'phone', 'address', 'shipping_name',
+            'shipping_address', 'shipping_phone', 'voucher_code');
         $content =Cart::content();
         $total=cart::total();
         return view('order/checkout')->with('content', $content)->with('total', $total)->with('order', $order);
@@ -22,26 +23,26 @@ class OrderController extends Controller
     public function store(Request $request)
     {
         $Order = new Order;
-        $total=cart::total();
-        if(Auth::check()){
+        $total=Cart::total();
+        if (Auth::check()) {
             $Order->name = Auth::user()->name;
             $Order->phone = Auth::user()->profile['phone'];
             $Order->address = Auth::user()->profile['address'];
-        }
+            }
         else {
             $Order->name = Input::get('name');
             $Order->phone = Input::get('phone');
             $Order->address = Input::get('address');
-        }
-        $Order->shipping_address 	= Input::get('shipping_address');
-        $Order->shipping_name 		= Input::get('shipping_name');
-        $Order->shipping_phone 	= Input::get('shipping_phone');
-        $Order->voucher_code 	= Input::get('voucher_code');
-        $Order->status 	= 0;
-        $Order->total_price 	= $total;
-        $Order->save();
-        $content =Cart::content();
-        $OrderProduct = new OrderProduct;
+             }
+            $Order->shipping_address 	= Input::get('shipping_address');
+            $Order->shipping_name 		= Input::get('shipping_name');
+            $Order->shipping_phone 	= Input::get('shipping_phone');
+            $Order->voucher_code 	= Input::get('voucher_code');
+            $Order->status 	= 0;
+            $Order->total_price 	= $total;
+            $Order->save();
+            $content =Cart::content();
+        $OrderProduct = new OrderProduct; 
         foreach ($content as $contents){
             $OrderProduct->quantity=$contents->qty;
             $OrderProduct->price=$contents->price;
--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,12 +9,10 @@ class ProductController extends Controller
 {
     public function productDetail($shopProductId)
     {
-        $content=Cart::content()->where('id',$shopProductId);
         $shopProduct = \App\ShopProduct::find($shopProductId);
         $attributes = $shopProduct->product->productAttributeValues;
         return view('productDetail')
             ->with('shopProduct', $shopProduct)
-            ->with('attributes', $attributes)
-            ->with('content', $content);
+            ->with('attributes', $attributes);
     }
 }
