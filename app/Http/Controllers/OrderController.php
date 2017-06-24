3d44c44b1c1f66de95a3e37fcae80270be7ca27f
doinguoi1232 <phuquydn@gmail.com>
2017-06-25 02:46:43 +0700

    shoppingproductdetail

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -25,21 +25,21 @@ class OrderController extends Controller
         $Order = new Order;
         $total=Cart::total();
         if (Auth::check()) {
-        $Order->name = Auth::user()->name;
-        $Order->phone = Auth::user()->profile['phone'];
-        $Order->address = Auth::user()->profile['address'];
+            $Order->name = Auth::user()->name;
+            $Order->phone = Auth::user()->profile['phone'];
+            $Order->address = Auth::user()->profile['address'];
         }
         else {
-        $Order->name = Input::get('name');
-        $Order->phone = Input::get('phone');
-        $Order->address = Input::get('address');
-         }
-        $Order->shipping_address 	= Input::get('shipping_address');
-        $Order->shipping_name 		= Input::get('shipping_name');
-        $Order->shipping_phone 	= Input::get('shipping_phone');
-        $Order->voucher_code 	= Input::get('voucher_code');
+            $Order->name = Input::get('name');
+            $Order->phone = Input::get('phone');
+            $Order->address = Input::get('address');
+        }
+        $Order->shipping_address = Input::get('shipping_address');
+        $Order->shipping_name = Input::get('shipping_name');
+        $Order->shipping_phone = Input::get('shipping_phone');
+        $Order->voucher_code = Input::get('voucher_code');
         $Order->status 	= 0;
-        $Order->total_price 	= $total;
+        $Order->total_price = $total;
         $Order->save();
         $content =Cart::content();
         $OrderProduct = new OrderProduct;
