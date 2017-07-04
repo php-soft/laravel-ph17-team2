fd55c86d7ce93f16cf12374a6b242ae47c6b1a61
Khoi Vinh Phan <khoivinhphan@gmail.com>
2017-07-05 00:31:03 +0700

    create_post_products #255

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -97,13 +97,12 @@ class ShopController extends Controller
     public function postProduct($id, Request $request)
     {
         $this->validate($request, [
-            'price' => 'required|numeric|max:6',
+            'price' => 'required|numeric',
             'quantity' => 'required|numeric',
             'discount' => 'required|numeric',
         ], [
             'price.required' => 'Bắt buộc phải điền giá thành',
             'price.numeric' => 'Giá phải là dạng số',
-            'price.max' => 'Không hơn 6 chữ số',
             'quantity.required' => 'Bắt buộc phải điền số lượng',
             'quantity.numeric' => 'Số lượng phải là dạng số',
             'discount.required' => 'Bắt buộc phải điền giảm giá',
