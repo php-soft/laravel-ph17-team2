106c7664460fc6f00ea4b7177c8e2627d5f5eb68
Thien Hoang <jaynup3ky@gmail.com>
2017-07-07 12:45:20 +0700

    Feat_show_news_page #265

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $news = \App\NewItem::with('user', 'newsCategory')->get();
+        $categories = \App\newsCategory::All();
+        return view('news/index')->with('news', $news)->with('categories', $categories);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($alias)
+    {
+        $new = \App\NewItem::where('alias', '=' ,$alias)->firstOrFail();
+        $categories = \App\newsCategory::All();   
+        return view('news/view')->with('new', $new)->with('categories', $categories);;
+    }
+
+    public function showcategory($alias)
+    {
+        $new = \App\NewItem::where('alias', '=' ,$alias)->firstOrFail();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/news/view.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+@section('content')
+	<div class="container-fluid">
+	<div class="row">
+	<div class="col-sm-9">
+		<ol class="breadcrumb">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Library</a></li>
+  <li class="active">Data</li>
+</ol>
+			<article class="row">
+	<div>
+
+
+			
+
+
+		<div class="col-sm-12">
+			<header>
+
+				<h5><a href="news/{!! $new->alias !!}">{!! $new->title !!}</a></h5>		
+				</header><!-- .entry-header -->
+
+			<div>
+				{!! $new->content !!}</div>
+				<div>
+					<div> 
+						<span class="glyphicon glyphicon-user"> Post: {!! $new->user->name !!}</span>
+						<span class="glyphicon glyphicon-calendar"> On: {!! $new->created_at!!}</span>
+						<span class="glyphicon glyphicon-folder-open"> Category: {!! $new->newsCategory->name !!}</span>				
+					</div>
+				</div>
+
+			
+
+		
+	</div>
+</div></article>
+	
+
+	</div>
+	<div class="col-sm-3">
+		<aside>
+			
+			<h5>Danh mục</h5>
+			<div class="list-group">
+			@foreach($categories as $category)
+				<a href="news/{!! $category->alias !!}" class="list-group-item">{!! $category->name !!}<span class="badge">14</span></a>
+			@endforeach 
+</div>
+		</aside>
+	</div>
+		
+
+
+	
+		
+	</div>
+		</div>
+@endsection
